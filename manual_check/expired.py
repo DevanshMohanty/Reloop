4586a1b73af0fcb724d82b0055786c4d92f16ac1
Arij Mookherjee <arij.mookherjee@gyansys.com>
2026-04-21 17:08:47 +0530

    Added Requirements and updated expired

--- manual_check/expired.py
+++ manual_check/expired.py
@@ -3,12 +3,9 @@ from prompts import ManualView_user_prompt
 from llm import llm
 
 def repurposes():
-    system_prompt = ManualView_Expired_system_prompt
-    user_prompt = ManualView_user_prompt
-    
     messages = [
-    {"role": "system", "content": system_prompt},
-    {"role": "user", "content": user_prompt}
+    {"role": "system", "content": ManualView_Expired_system_prompt},
+    {"role": "user", "content": ManualView_user_prompt}
     ]
 
     str = llm(messages)
