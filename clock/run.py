5e4e76a9b014eba7c5ff686b4cb8c5e14d15d113
Arij Mookherjee <arij.mookherjee@gyansys.com>
2026-04-23 19:22:49 +0530

    Minor changes(no islands involved)

--- clock/run.py
+++ clock/run.py
@@ -1,5 +1,6 @@
 import schedule
 import time
+from datetime import datetime
 from db_scripts.update_db import daily_update
 from daily_check.main import daily_check
 import sqlite3
@@ -11,21 +12,21 @@ cursor=conn.cursor()
 
 
 def job1():#only to update the sql database
-    print(f"{time.time}:Updating DB")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -Updating DB")
     daily_update(time.time)
-    print(f"{time.time}:Generating json representation of DB")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -Generating json representation of DB")
     verify()
     
     
 def job2():#only yields notifications
-    print(f"{time.time}:Delivering Notifications")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -Delivering Notifications")
     cursor.execute("SELECT prod_type, days_left FROM products")
     rows = cursor.fetchall()
     threshold = 3
     i = 1
     for prod_type,days_left in rows:
         notif = daily_check(threshold,int(days_left),prod_type)
-        print(f"{time.time}:NOTIFICATION {i}")
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} -NOTIFICATION {i}")
         print(f'\n{notif}')
         i+=1
     
